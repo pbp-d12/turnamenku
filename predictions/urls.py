7b562eec0b70694310cbbe10dde63ecc00b14eef
mugmold <mugmold@gmail.com>
2025-10-22 21:25:33 +0700

    lmao

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from . import views # (Atau import views kamu)
+
+# Ini WAJIB ADA, meskipun masih kosong
+urlpatterns = [
+    # Nanti kamu bisa tambahkan path kamu di sini
+    # Contoh: path('', views.index, name='index'),
+]
