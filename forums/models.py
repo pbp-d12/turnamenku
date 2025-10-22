8e5b6c94ef79f9c87d38735b766a5e9c6b956179
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-10-22 12:23:35 +0700

    feat(forums): Menambahkan app forums

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from main.models import User
+from tournaments.models import Tournament
+import uuid
 
-# Create your models here.
+
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, related_name='threads', on_delete=models.CASCADE)
+    tournament = models.ForeignKey(Tournament, related_name='threads', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thread = models.ForeignKey(Thread, related_name='posts', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Post in {self.thread.title} at {self.created_at}'
+    
