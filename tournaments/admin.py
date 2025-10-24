80defe72e2138306bd4a6abfa9b000104d197d29
unknown <gibran.021105@gmail.com>
2025-10-24 23:36:19 +0700

    Daftar Peserta

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -3,14 +3,14 @@ from .models import Tournament, Match
 
 @admin.register(Tournament)
 class TournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'organizer', 'start_date', 'end_date') # Fields to show in the list view
-    list_filter = ('start_date', 'end_date', 'organizer') # Fields to filter by
-    search_fields = ('name', 'description', 'organizer__username') # Fields to search by
-    date_hierarchy = 'start_date' # Adds date drill-down navigation
+    list_display = ('name', 'organizer', 'start_date', 'end_date') 
+    list_filter = ('start_date', 'end_date', 'organizer') 
+    search_fields = ('name', 'description', 'organizer__username') 
+    date_hierarchy = 'start_date' 
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'tournament', 'match_date', 'home_score', 'away_score') # Use the model's __str__ method
+    list_display = ('__str__', 'tournament', 'match_date', 'home_score', 'away_score') 
     list_filter = ('tournament', 'match_date', 'home_team', 'away_team')
     search_fields = ('tournament__name', 'home_team__name', 'away_team__name')
     date_hierarchy = 'match_date'
